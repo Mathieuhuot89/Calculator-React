0eb0336e6cccd59e6d3c998b107e8678d89a2b8b
Mathieu <huot.mathieuu@gmail.com>
2023-04-01 17:55:23 +0200

    first commit

---
+++ src/components/Wrapper.js
@@ -0,0 +1,13 @@
+import './Wrapper.css';
+
+import React from 'react'
+
+const Wrapper = ({ children }) => {
+  return (
+    <div className='wrapper'>
+      {children}
+    </div>
+  )
+}
+
+export default Wrapper
